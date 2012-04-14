3002db1fe79147225322d0e9eee7ea7261c9b71e
Nicolas OGIER <nicolas.ogier@gmail.com>
2012-04-14 11:26:59 +0200

    Version 1.0 du plugin

---
+++ README.markdown
@@ -0,0 +1,20 @@
+# UnpublishFront for Joomla
+## Plugin for Joomla 2.5
+This plugin unset frontpage status on articles, after x days from created, published or last modify date.
+Based on  Dmitry V. Smirnov plugin for Joomla 1.5 (http://www.joomlatune.ru)
+
+## Add it to your site
+
+### Step 1
+
+Download plugin file
+
+### Step 2
+
+In Joomla administration, set Extensions > Install
+
+
+### Step 3
+
+Initialize the plugin in Extensions > Plugin
+Switch this plugin System active and Set up.
---
+++ unpublishfront.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *  @Copyright   Copyright (C) 2012 by Nicolas OGIER www.nicolas-ogier.fr
+ *  @package     Unpublishfront -  unpublish articles from frontpage for Joomla 2.5
+ *  @author      Nicolas OGIER {@link http://www.nicolas-ogier.fr}
+ *  				based on Dmitry V. Smirnov (http://www.joomlatune.ru) plugin for Joomla 1.5
+ *  @version     2.5-1.0 - 06-April-2012
+ *  @link        http://www.nicolas-ogier.fr/download/unpublishfront.html
+ *
+ *  @license GNU/GPL
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+// protection de la page
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.plugin.plugin');
+class plgSystemUnpublishfront extends JPlugin
+{
+	/**
+	* @param	object		$subject The object to observe
+	* @param 	array  		$config  An array that holds the plugin configuration
+	* @since	1.0
+	*/
+	function plgSystemUnpublishfront(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+	}
+	public function OnAfterInitialise()
+	{
+		$days 		= $this->params->get('days',30);
+		$startpoint = $this->params->get('startpoint','created');
+		$excludes 	= $this->params->get('excludes',0);
+		
+		// Get a database object
+		$db =& JFactory::getDBO();
+		// default query
+		$query = "SELECT id FROM #__content WHERE TO_DAYS( NOW( ) ) - TO_DAYS( `created` ) >=".$days.' AND id NOT IN('.$excludes.')';
+		// query addapted
+		if($startpoint=='created') {
+			$query = "SELECT id FROM #__content WHERE TO_DAYS( NOW( ) ) - TO_DAYS( `created` ) >=".$days.' AND id NOT IN('.$excludes.')';
+			}
+		else {
+			if ($startpoint=='modified') {
+				$query = "SELECT id FROM #__content WHERE TO_DAYS( NOW( ) ) - TO_DAYS( `modified` ) >=".$days.' AND id NOT IN('.$excludes.')';
+			}
+			 else {
+			 	if($startpoint=="publish_up") {
+			 		$query = "SELECT id FROM #__content WHERE TO_DAYS( NOW( ) ) - TO_DAYS(  `publish_up` ) >=".$days.' AND id NOT IN('.$excludes.')';
+			 	}
+			 }
+		}
+		$db->setQuery($query);
+		$rows = $db->loadResultArray();
+		//print_r($rows);	
+		$daystring = implode(',' , $rows);
+	
+		$query = "DELETE FROM #__content_frontpage WHERE content_id IN($daystring)";
+		$db->setQuery($query);
+		$db->Query();
+		$query = "UPDATE  #__content SET  `featured` =  0 WHERE  id IN($daystring)";
+		$db->setQuery($query);
+		$db->Query();
+		
+	}
+}
